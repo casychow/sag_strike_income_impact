6f26d7fd803418026e1a592d47f97a4d52245ad1
casychow <cc5718@nyu.edu>
2023-09-08 13:58:37 -0400

    added todo list; space labels and entry boxes based on feature; add monthly deduction label and entry box; did not connect monthly deduction to total amount yet

--- tkinter_sag_strike_calculator.py
+++ tkinter_sag_strike_calculator.py
@@ -4,6 +4,15 @@
 # to input their expected work hours and constant hourly rate to estimate a
 # projected loss of income while protesting unfair labor laws.
 
+"""
+To-Do:
+- [] insert last month's tax -> get actual pay and %loss
+- [] remove all unnecessary "self."
+- [] make moving table entries easier when adding new features/boxes instead of recalculating all object placement
+- [] table to insert estimated pay for which dates, taxes, actual pay, % loss, payday
+- [] calculate average tax -> use to tune actual pay and % loss
+"""
+
 import tkinter as tk
 from tkinter import messagebox
 
@@ -25,23 +34,35 @@ class Window():
         self.reg_hr_rate = tk.StringVar()
         self.over_hrs = tk.StringVar()
         self.over_hr_rate = tk.StringVar()
+        self.monthly_deductions = tk.StringVar()
+
+        """
+        For:
+        - Expected Regular Work Hour(s)
+        - Hourly Rate
+        - Expected Overtime Work Hour(s)
+        - Overtime Rate
+        - Last month's deductions
+        - Answer
+        """
 
         # Show labels and entry boxes
         self.reg_hr_label = tk.Label(self.window, text="Expected Regular Work Hour(s): ") # Show following "Expected Work Hours" entry box
         self.reg_hr_entry = tk.Entry(self.window, textvariable=self.reg_hrs) # Input expected work hours
+
         self.reg_hr_rate_label = tk.Label(self.window, text="Hourly Rate: ") # Show following "Hourly Rate" entry box
         self.reg_hr_rate_entry = tk.Entry(self.window, textvariable=self.reg_hr_rate) # Input hourly work rate
+
         self.over_hr_label = tk.Label(self.window, text="Expected Overtime Work Hour(s): ") # Show following "Expected Overtime Work Hour(s)" entry box
         self.over_hr_entry = tk.Entry(self.window, textvariable=self.over_hrs) # Input expected overtime work hours
+
         self.over_hr_rate_label = tk.Label(self.window, text="Overtime Rate: ") # Show following "Overtime Rate" entry box
         self.over_hr_rate_entry = tk.Entry(self.window, textvariable=self.over_hr_rate) # Input hourly overtime work rate
-        self.answer = tk.Label(self.window, text="{0}: $0.0".format(self.message)) # Show following total expected losses
 
-        # Initialize and place calculate and quit buttons
-        calc_button = tk.Button(self.window, text="Calculate", command=self.calculate)
-        calc_button.place(x=185, y=190)
-        quit_button = tk.Button(self.window, text="Quit", command=self.program_exit)
-        quit_button.place(x=185, y=250)
+        self.monthly_deduction_label = tk.Label(self.window, text="Monthly Deduction: ") # Show following "Monthly Deduction" entry box
+        self.monthly_deduction_entry = tk.Entry(self.window, textvariable=self.monthly_deductions) # Input last month's deduction (from tax, 401k contribution, etc)
+
+        self.answer = tk.Label(self.window, text="{0}: $0.0".format(self.message)) # Show following total expected losses
 
         # Place all objects in appropriate locations in window
         self.reg_hr_label.place(x=10, y=25)
@@ -52,7 +73,15 @@ class Window():
         self.over_hr_entry.place(x=200, y=75)
         self.over_hr_rate_label.place(x=10, y=100)
         self.over_hr_rate_entry.place(x=200, y=100)
-        self.answer.place(x=200, y=150)
+        self.monthly_deduction_label.place(x=10, y=125)
+        self.monthly_deduction_entry.place(x=200, y=125)
+        self.answer.place(x=175, y=150)
+
+        # Initialize and place calculate and quit buttons
+        calc_button = tk.Button(self.window, text="Calculate", command=self.calculate)
+        calc_button.place(x=175, y=190)
+        quit_button = tk.Button(self.window, text="Quit", command=self.program_exit)
+        quit_button.place(x=175, y=250)
 
     def calculate(self) -> None:
         """
@@ -60,9 +89,9 @@ class Window():
 
         Formula = reg_work_hrs * reg_hrly_rate + overtime_hrs * overtime_hrly_rate
         """
-        answer = round(float(self.reg_hrs.get()) * float(self.reg_hr_rate.get()) + float(self.over_hrs.get()) * float(self.over_hr_rate.get()), 2)
-        self.answer.config(text="{0}: ${1}".format(self.message, answer))
-        messagebox.showinfo("Projected Impact", "{0}${1}".format(self.message, answer))
+        money_total = round(float(self.reg_hrs.get()) * float(self.reg_hr_rate.get()) + float(self.over_hrs.get()) * float(self.over_hr_rate.get()), 2)
+        self.answer.config(text="{0}: ${1}".format(self.message, money_total))
+        messagebox.showinfo("Projected Impact", "{0}${1}".format(self.message, money_total))
 
     def program_exit(self) -> None:
         """
