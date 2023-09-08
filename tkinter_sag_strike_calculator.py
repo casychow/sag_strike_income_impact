fe9b449622669f5d2d0305abb8951b54469c3ade
casychow <cc5718@nyu.edu>
2023-09-08 15:57:09 -0400

    add pay_deductions and actual_paid entries; calculated and showed percent loss; displayed everything in text box below; renamed variables

--- tkinter_sag_strike_calculator.py
+++ tkinter_sag_strike_calculator.py
@@ -6,7 +6,6 @@
 
 """
 To-Do:
-- [] insert last month's tax -> get actual pay and %loss
 - [] remove all unnecessary "self."
 - [] make moving table entries easier when adding new features/boxes instead of recalculating all object placement,
     OR
@@ -16,7 +15,7 @@ To-Do:
 """
 
 import tkinter as tk
-from tkinter import messagebox
+# from tkinter import messagebox
 
 class Window():
     def __init__(self) -> None:
@@ -26,17 +25,18 @@ class Window():
         # Setup tkinter window
         self.window = tk.Tk()
         self.window.title("SAG-AFTRA Strike Estimated Cost Calculator")
-        self.window.geometry("500x300")
+        self.window.geometry("600x450")
 
         # Setup text
-        self.message = "Projected Impact on Income: "
+        self.loss_income_message = "Projected Impact on Income: "
 
         # Declare string variables for storage
-        self.reg_hrs = tk.StringVar()
-        self.reg_hr_rate = tk.StringVar()
-        self.over_hrs = tk.StringVar()
-        self.over_hr_rate = tk.StringVar()
-        self.monthly_deductions = tk.StringVar()
+        self.reg_hrs_string = tk.StringVar()
+        self.reg_hr_rate_string = tk.StringVar()
+        self.over_hrs_string = tk.StringVar()
+        self.over_hr_rate_string = tk.StringVar()
+        self.pay_deductions_string = tk.StringVar()
+        self.paid_amount_string = tk.StringVar()
 
         """
         For:
@@ -50,21 +50,24 @@ class Window():
 
         # Show labels and entry boxes
         self.reg_hr_label = tk.Label(self.window, text="Expected Regular Work Hour(s): ") # Show following "Expected Work Hours" entry box
-        self.reg_hr_entry = tk.Entry(self.window, textvariable=self.reg_hrs) # Input expected work hours
+        self.reg_hr_entry = tk.Entry(self.window, textvariable=self.reg_hrs_string) # Input expected work hours
 
         self.reg_hr_rate_label = tk.Label(self.window, text="Hourly Rate: ") # Show following "Hourly Rate" entry box
-        self.reg_hr_rate_entry = tk.Entry(self.window, textvariable=self.reg_hr_rate) # Input hourly work rate
+        self.reg_hr_rate_entry = tk.Entry(self.window, textvariable=self.reg_hr_rate_string) # Input hourly work rate
 
         self.over_hr_label = tk.Label(self.window, text="Expected Overtime Work Hour(s): ") # Show following "Expected Overtime Work Hour(s)" entry box
-        self.over_hr_entry = tk.Entry(self.window, textvariable=self.over_hrs) # Input expected overtime work hours
+        self.over_hr_entry = tk.Entry(self.window, textvariable=self.over_hrs_string) # Input expected overtime work hours
 
         self.over_hr_rate_label = tk.Label(self.window, text="Overtime Rate: ") # Show following "Overtime Rate" entry box
-        self.over_hr_rate_entry = tk.Entry(self.window, textvariable=self.over_hr_rate) # Input hourly overtime work rate
+        self.over_hr_rate_entry = tk.Entry(self.window, textvariable=self.over_hr_rate_string) # Input hourly overtime work rate
 
-        self.monthly_deduction_label = tk.Label(self.window, text="Monthly Deduction: ") # Show following "Monthly Deduction" entry box
-        self.monthly_deduction_entry = tk.Entry(self.window, textvariable=self.monthly_deductions) # Input last month's deduction (from tax, 401k contribution, etc)
+        self.pay_deduction_label = tk.Label(self.window, text="Pay Deduction: ") # Show following "Pay Deduction" entry box
+        self.pay_deduction_entry = tk.Entry(self.window, textvariable=self.pay_deductions_string) # Input last pay stub's deductions (from tax, 401k contribution, etc)
 
-        self.answer = tk.Label(self.window, text="{0}: $0.0".format(self.message)) # Show following total expected losses
+        self.paid_amount_label = tk.Label(self.window, text="Actual Pay: ")
+        self.paid_amount_entry = tk.Entry(self.window, textvariable=self.paid_amount_string) # Input last pay stub's final paid amount
+
+        self.answer = tk.Label(self.window, text="{0} $0.0".format(self.loss_income_message)) # Show following total expected losses
 
         # Place all objects in appropriate locations in window
         self.reg_hr_label.place(x=10, y=25)
@@ -75,27 +78,40 @@ class Window():
         self.over_hr_entry.place(x=200, y=75)
         self.over_hr_rate_label.place(x=10, y=100)
         self.over_hr_rate_entry.place(x=200, y=100)
-        self.monthly_deduction_label.place(x=10, y=125)
-        self.monthly_deduction_entry.place(x=200, y=125)
-        self.answer.place(x=175, y=150)
+        self.pay_deduction_label.place(x=10, y=125)
+        self.pay_deduction_entry.place(x=200, y=125)
+        self.paid_amount_label.place(x=10, y=150)
+        self.paid_amount_entry.place(x=200, y=150)
+        self.answer.place(x=100, y=250)
 
         # Initialize and place calculate and quit buttons
         calc_button = tk.Button(self.window, text="Calculate", command=self.calculate)
-        calc_button.place(x=175, y=190)
+        calc_button.place(x=350, y=25)
         quit_button = tk.Button(self.window, text="Quit", command=self.program_exit)
-        quit_button.place(x=175, y=250)
+        quit_button.place(x=350, y=50)
 
     def calculate(self) -> None:
         """
         Calculate the estimated loss of money for the user based on the following formula.
 
-        estimated_pay = reg_work_hrs * reg_hrly_rate + overtime_hrs * overtime_hrly_rate
-        actual_pay = estimated_pay - monthly_deductions
+        Formulas:
+        - Estimated Pay = (Regular Work Hours * Regular Hourly Rate) + (Overtime Hours * Overtime Hourly Rate)
+        - % loss = (Estimated Pay - Actual Pay) / Estimated Pay
         """
-        estimated_pay = round(float(self.reg_hrs.get()) * float(self.reg_hr_rate.get()) + float(self.over_hrs.get()) * float(self.over_hr_rate.get()), 2)
-        actual_pay = estimated_pay - round(float(self.monthly_deductions.get()), 2)
-        self.answer.config(text="{0}: ${1}".format(self.message, actual_pay))
-        messagebox.showinfo("Projected Impact", "{0}${1}".format(self.message, actual_pay))
+        # What if some fields are left blank? - make them equal to 0 if nothing is in there
+
+        estimated_pay = round(float(self.reg_hrs_string.get()) * float(self.reg_hr_rate_string.get()) + float(self.over_hrs_string.get()) * float(self.over_hr_rate_string.get()), 2)
+        deducted_pay = float(self.pay_deductions_string.get())
+        actual_pay = float(self.paid_amount_string.get())
+        percent_loss = round(((estimated_pay - actual_pay) / estimated_pay * 100), 2)
+        self.answer.config(text="{} {}".format(self.loss_income_message, actual_pay))
+        calculated_info = "Estimated Pay: ${0}\nPay Stub Deductions: ${1}\nActual Pay: ${2}\nPercent Loss: {3}%".format(estimated_pay, deducted_pay, actual_pay, percent_loss)
+        
+        final_text = tk.Text(self.window, height = 5, width = 40)
+        final_text.place(x=100, y=300)
+        final_text.insert(tk.END, calculated_info)
+
+        # messagebox.showinfo("{0}${1}".format(self.message, actual_pay))
 
     def program_exit(self) -> None:
         """
